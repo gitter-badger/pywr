gitter-badger/pywr

commit 5dff5969fd19a6feeaf32d0a6b5dd23e5eaf0f90

Optimised SolverGLPK by adding dirty flag to model structure.

pywr/solvers/pywr_glpk.py
@@ -6,81 +6,122 @@ import glpk
 
 inf = float('inf')
 
+
 class SolverGLPK(Solver):
     name = 'GLPK'
+
     def solve(self, model):
+        if model.dirty:
+            '''
+            This section should only need to be run when the model has changed
+            structure (e.g. a new connection has been made).
+            '''
+
+            lp = self.lp = glpk.LPX()
+            lp.obj.maximize = True
+
+            # find all routes betwee supply and demand nodes
+            routes = self.routes = model.find_all_routes(Supply, Demand, valid=(Link,))
+
+            # each route between a supply and demand is represented as a column
+            supply_nodes = self.supply_nodes = {}
+            demand_nodes = self.demand_nodes = {}
+            lp.cols.add(len(routes))
+            for col_idx, route in enumerate(routes):
+                col = lp.cols[col_idx]
+                col.bounds = 0, None  # supply must be >= 0
+                supply_node = route[0]
+                demand_node = route[-1]
+
+                supply_nodes.setdefault(supply_node, {'cols': [], 'col_idxs': []})
+                supply_nodes[supply_node]['cols'].append(col)
+                supply_nodes[supply_node]['col_idxs'].append(col_idx)
+
+                demand_nodes.setdefault(demand_node, {'cols': [], 'col_idxs': []})
+                demand_nodes[demand_node]['cols'].append(col)
+                demand_nodes[demand_node]['col_idxs'].append(col_idx)
+
+                intermediate_max_flow_constraints = self.intermediate_max_flow_constraints = {}
+                intermediate_nodes = route[1:-1]
+                for node in intermediate_nodes:
+                    if 'max_flow' in node.properties:
+                        row_idx = lp.rows.add(1)
+                        row = lp.rows[row_idx]
+                        row.matrix = [(idx, 1.0) for idx in route_idxs]
+                        intermediate_max_flow_constraints[node] = row
+
+            for supply_node, info in supply_nodes.items():
+                row_idx = lp.rows.add(1)
+                row = lp.rows[row_idx]
+                info['supply_constraint'] = row
+                info['matrix'] = [(idx, 1.0) for idx in info['col_idxs']]
+
+                if isinstance(supply_node, RiverAbstraction):
+                    row_idx = lp.rows.add(1)
+                    row = lp.rows[row_idx]
+                    info['river_constraint'] = row
+                    info['river_routes'] = river_routes = model.find_all_routes(Catchment, supply_node)
+
+            for demand_node, info in demand_nodes.items():
+                row_idx = lp.rows.add(1)
+                row = lp.rows[row_idx]
+                info['demand_constraint'] = row
+                info['matrix'] = [(idx, 1.0) for idx in info['col_idxs']]
+
+            model.dirty = False
+        else:
+            lp = self.lp
+            supply_nodes = self.supply_nodes
+            demand_nodes = self.demand_nodes
+            routes = self.routes
+            intermediate_max_flow_constraints = self.intermediate_max_flow_constraints
+
         timestamp = model.timestamp
-        
-        routes = model.find_all_routes(Supply, Demand, valid=(Link,))
-        count_routes = len(routes)
-        assert(count_routes > 0)
-        
+
+        # apply route costs as objective function
         costs = []
-        by_supply = {}
-        by_demand = {}
-        for n, route in enumerate(routes):
-            supply_node = route[0]
-            demand_node = route[-1]
-            by_supply.setdefault(supply_node, [])
-            by_supply[supply_node].append(n)
-            by_demand.setdefault(demand_node, [])
-            by_demand[demand_node].append(n)
+        for col_idx, route in enumerate(routes):
             cost = 0.0
             for node in route:
                 cost += node.properties['cost'].value(timestamp)
             costs.append(cost)
+        max_cost = max(costs)
+        for col_idx, cost in enumerate(costs):
+            lp.obj[col_idx] = 1 + max_cost - cost
 
-        lp = glpk.LPX()
-        
-        # add a column for each route
-        lp.cols.add(count_routes)
-        for n, route in enumerate(routes):
-            col = lp.cols[n]
-            col.bounds = 0, inf
-        
-        for supply_node, idxs in by_supply.items():
-            # maximum supply from node is limited by max_flow parameter and licenses
+        # supply is limited by a maximum flow, and any licenses
+        for supply_node, info in supply_nodes.items():
+            row = info['supply_constraint']
             max_flow_parameter = supply_node.properties['max_flow'].value(timestamp)
             max_flow_license = inf
             if supply_node.licenses is not None:
                 max_flow_license = supply_node.licenses.available(timestamp)
             max_flow = min(max_flow_parameter, max_flow_license)
-            lp.rows.add(1)
-            row = lp.rows[-1]
-            row.matrix = [(idx, 1.0) for idx in idxs]
+            row.matrix = info['matrix']
             row.bounds = 0, max_flow
 
+        # demands require water, but can only accept a certain amount
         total_water_demanded = 0.0
-        for demand_node, idxs in by_demand.items():
+        for demand_node, info in demand_nodes.items():
+            row = info['demand_constraint']
             demand_value = demand_node.properties['demand'].value(timestamp)
-            lp.rows.add(1)
-            row = lp.rows[-1]
-            row.matrix = [(idx, 1.0) for idx in idxs]
+            row.matrix = info['matrix']
             row.bounds = 0, demand_value
             total_water_demanded += demand_value
-        
+
         # intermediate node max flow constraints
-        max_flow_constraints = {}
-        for n, route in enumerate(routes):
-            intermediate_nodes = route[1:-1]
-            for node in intermediate_nodes:
-                if 'max_flow' in node.properties:
-                    max_flow_constraints.setdefault(node, [])
-                    max_flow_constraints[node].append(n)
-        for node, route_idxs in max_flow_constraints.items():
-            lp.rows.add(1)
-            row = lp.rows[-1]
-            row.matrix = [(idx, 1.0) for idx in route_idxs]
+        for node, row in intermediate_max_flow_constraints.items():
             row.bounds = 0, node.properties['max_flow'].value(timestamp)
 
         # river flow constraints
-        for supply_node, idxs in by_supply.items():
+        for supply_node, info in supply_nodes.items():
             if isinstance(supply_node, RiverAbstraction):
                 flow_constraint = 0.0
                 # find all routes from a catchment to the abstraction
-                river_routes = model.find_all_routes(Catchment, supply_node)
+                river_routes = info['river_routes']
                 upstream_abstractions = {}
                 for route in river_routes:
+                    # traverse the route from abstraction back up to catchments
                     route = route[::-1]
                     coefficient = 1.0
                     for n, node in enumerate(route):
@@ -102,24 +143,30 @@ class SolverGLPK(Solver):
                 abstraction_idxs = []
                 abstraction_coefficients = []
                 for upstream_node, coefficient in upstream_abstractions.items():
-                    cols = by_supply[upstream_node]
+                    cols = supply_nodes[upstream_node]['col_idxs']
                     abstraction_idxs.extend(cols)
                     abstraction_coefficients.extend([coefficient]*len(cols))
-                lp.rows.add(1)
-                row = lp.rows[-1]
+                row = info['river_constraint']
                 row.matrix = [(abstraction_idxs[n], abstraction_coefficients[n]) for n in range(0, len(abstraction_idxs))]
                 row.bounds = 0, flow_constraint
-        
-        lp.obj.maximize = True
-        for n in range(len(lp.cols)):
-            lp.obj[n] = 1 + max(costs) - costs[n]
-        
-        lp.simplex()
-        
-        result = [round(value.primal, 3) for value in lp.cols]
 
+        # solve the linear programme
+        lp.simplex()
+        assert(lp.status == 'opt')
+        status = 'optimal'
+
+        # retrieve the results
+        result = [round(value.primal, 3) for value in lp.cols]
         total_water_supplied = sum(result)
 
+        # commit the volume of water actually supplied
+        for n, route in enumerate(routes):
+            route[0].commit(result[n], chain='first')
+            for node in route[1:-1]:
+                node.commit(result[n], chain='middle')
+            route[-1].commit(result[n], chain='last')
+
+        # calculate the total amount of water transferred via each node/link
         volumes_links = {}
         volumes_nodes = {}
         for n, route in enumerate(routes):
@@ -130,36 +177,21 @@ class SolverGLPK(Solver):
 
                     if m+1 == len(route):
                         break
-                    
+
                     pair = (route[m], route[m+1])
                     volumes_links.setdefault(pair, 0.0)
                     volumes_links[pair] += result[n]
-        
-        # commit the volume of water actually supplied
-        for n, route in enumerate(routes):
-            route[0].commit(result[n], chain='first')
-            for node in route[1:-1]:
-                node.commit(result[n], chain='middle')
-            route[-1].commit(result[n], chain='last')
-
-        for k,v in volumes_links.items():
-            v = round(v,3)
+        for k, v in volumes_links.items():
+            v = round(v, 3)
             if v:
                 volumes_links[k] = v
             else:
                 del(volumes_links[k])
-        
-        for k,v in volumes_nodes.items():
-            v = round(v,3)
+        for k, v in volumes_nodes.items():
+            v = round(v, 3)
             if v:
                 volumes_nodes[k] = v
             else:
                 del(volumes_nodes[k])
-        
-        status = lp.status
-        if status == 'opt':
-            status = 'optimal'
-        
-        assert(status == 'optimal')
-        
+
         return status, round(total_water_demanded, 3), round(total_water_supplied, 3), volumes_links, volumes_nodes

pywr/core.py
@@ -25,6 +25,7 @@ class Model(object):
         self.metadata = {}
         self.parameters = {}
         self.data = {}
+        self.dirty = True
         
         if solver is not None:
             # use specific solver

commit 52f557596c35d4d118aaa93668a5f628f14a8e96

Fixed regression in 5dff596 with intermediate max flow constraint
Added a test case which triggers the issue.

pywr/solvers/pywr_glpk.py
@@ -44,11 +44,15 @@ class SolverGLPK(Solver):
                 intermediate_max_flow_constraints = self.intermediate_max_flow_constraints = {}
                 intermediate_nodes = route[1:-1]
                 for node in intermediate_nodes:
-                    if 'max_flow' in node.properties:
+                    if 'max_flow' in node.properties and node not in intermediate_max_flow_constraints:
                         row_idx = lp.rows.add(1)
                         row = lp.rows[row_idx]
-                        row.matrix = [(idx, 1.0) for idx in route_idxs]
                         intermediate_max_flow_constraints[node] = row
+                        col_idxs = []
+                        for col_idx, route in enumerate(routes):
+                            if node in route:
+                                col_idxs.append(col_idx)
+                        row.matrix = [(idx, 1.0) for idx in col_idxs]
 
             for supply_node, info in supply_nodes.items():
                 row_idx = lp.rows.add(1)

tests/test_run.py
@@ -149,6 +149,14 @@ def test_run_license():
     assert(result[0:3] == ('optimal', 10.0, 0.0))
     assert(annual_lic.resource_state(model.timestamp) == 0.0)
 
+def test_run_bottleneck():
+    '''Test max flow constraint on intermediate nodes is upheld'''
+    data = file(os.path.join(os.path.dirname(__file__), 'bottleneck.xml'), 'r').read()
+    model = pywr.xmlutils.parse_xml(data)
+    model.check()
+    result = model.step()
+    assert(result[0:3] == ('optimal', 20.0, 15.0))
+
 def test_solver_glpk():
     '''Test specifying the solver in XML'''
     data = '''<pywr><solver name="glpk" /><nodes /><edges /><metadata /></pywr>'''